4b4fd4d3e2d9088058f124b6e40517bd97f525d4
Purba Mears <purba.mears@gmail.com>
2016-09-12 20:40:23 -0400

    initial commit

---
+++ labs/ArrayExample.java
@@ -0,0 +1,25 @@
+package labs;
+import java.util.Scanner;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String [] names = {"Harry", "Harmonie","Ron","Ginny","Neville"};
+		for (int i =0; i<names.length;i++){
+			System.out.println((i+1) +". " + names[i]);
+		}
+		
+	//enhanced for loop
+	for( string names : studentNames){
+		System.out.println(studentNames);
+	}
+	
+	
+	
+	}
+
+	
+	
+}
